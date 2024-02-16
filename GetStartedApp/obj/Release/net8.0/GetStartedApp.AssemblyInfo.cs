629eec95b1064c8f4f9c29ed81396f6f91e1384f
danbennett239 <danieljbennett@ymail.com>
2024-02-16 00:12:00 +0000

    Create Initial Layout

--- GetStartedApp/obj/Release/net8.0/GetStartedApp.AssemblyInfo.cs
+++ GetStartedApp/obj/Release/net8.0/GetStartedApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80eb71b0e0aa9606c64549497928f95ffff251b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
