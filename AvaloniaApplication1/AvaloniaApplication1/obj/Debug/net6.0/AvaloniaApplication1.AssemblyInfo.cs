9e4baa080840fb096cb4f678963c692c399fc9f4
danbennett239 <danieljbennett@ymail.com>
2024-02-16 23:13:36 +0000

    Add Volume Bar to UI

--- AvaloniaApplication1/AvaloniaApplication1/obj/Debug/net6.0/AvaloniaApplication1.AssemblyInfo.cs
+++ AvaloniaApplication1/AvaloniaApplication1/obj/Debug/net6.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80eb71b0e0aa9606c64549497928f95ffff251b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08424c1d5d17b871470158be92ab5329e8e7650")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
